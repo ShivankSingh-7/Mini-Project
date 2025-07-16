950fe2439c7febf4ae32808251c8cdbf48a66876
Shivank Singh <shivangsingh819@gmail.com>
2025-07-16 21:31:19 +0530

    implemented main features

--- To-DO-List/script.js
+++ To-DO-List/script.js
@@ -1,8 +1,9 @@
 flatpickr("#calendar", {
-    inline: true // makes calendar always visible
+    inline: true
 });
 
 let eIndex = null;
+
 
 document.querySelectorAll(".plus, .addbox").forEach(el => {
     el.addEventListener("click", () => {
@@ -16,7 +17,7 @@ let notes = JSON.parse(localStorage.getItem("notes")) || [];
 
 
 
-function renderBox(title, description, index){
+function renderBox(title, description, index) {
     let el = document.createElement("li");
     el.innerHTML = `<div class="box">
                             <div class="writtenContent">
@@ -25,12 +26,12 @@ function renderBox(title, description, index){
                             </div>
                             <div class="icons">
                                 <img src="edit.svg" alt="edit icon">
-                                <img src="bin.svg" alt="bin icon">
+                                <img class="bin" src="bin.svg" alt="bin icon" data-index="${index}">
                             </div>
                         </div>`
 
     let box = el.querySelector(".box");
-    box.addEventListener("click",()=>{
+    box.addEventListener("click", () => {
         document.getElementById("ftitle").value = title;
         document.getElementById("description").value = description;
         document.querySelector(".task-detail-panel").classList.remove("display");
@@ -38,35 +39,65 @@ function renderBox(title, description, index){
         eIndex = index;
     });
 
+    el.querySelector(".bin").addEventListener("click", (e) => {
+        e.stopPropagation();
+        let deleteIndex = parseInt(e.target.dataset.index);
+        notes.splice(deleteIndex, 1);
+        localStorage.setItem("notes", JSON.stringify(notes));
+        document.querySelector("ul").innerHTML = "";
+        if (notes.length != 0) {
+            notes.forEach((n, i) => {
+                renderBox(n.title, n.description, i);
+            })
+        }
+        else{
+            document.querySelector(".plus").style.display = "block";
+        }
+    });
+
     document.querySelector("ul").appendChild(el);
     document.querySelector(".plus").style.display = "none";
 }
 
-notes.forEach((note, index) => {
-    renderBox(note.title, note.description, index);
-});
+if (notes.length != 0) {
+    notes.forEach((note, index) => {
+        renderBox(note.title, note.description, index);
+    });
+}
 
-document.querySelector(".done").addEventListener("click", ()=>{
+document.querySelector(".done").addEventListener("click", () => {
     let title = document.getElementById("ftitle").value;
     let description = document.getElementById("description").value;
+    let note = { title, description };
+    if (eIndex == null) {
+        notes.push(note);
+    }
+    else {
+        notes[eIndex] = note;
+    }
 
-    let note = {title, description};
-
-    notes.push(note);
-
-    localStorage.setItem("notes",JSON.stringify(notes));
+    localStorage.setItem("notes", JSON.stringify(notes));
 
     let a = document.querySelector(".task-detail-panel");
     document.getElementById("ftitle").value = "";
     document.getElementById("description").value = "";
+    eIndex = null;
     a.classList.toggle("display");
-    renderBox(title,description);
+    document.querySelector("ul").innerHTML = "";
+    notes.forEach((note, index) => {
+        renderBox(note.title, note.description, index);
+    })
+
+    console.log("clicked done");
 })
 
 
-function clock(hour, min, sec){
+function clock(hour, min, sec) {
+    if(hour>12){
+        hour = hour - 12;
+    }
     let box = document.querySelector(".clock-box");
-    box.innerHTML = `<p>${hour}: ${min}: ${sec}</p>`
+    box.innerHTML = `<p>${hour} : ${min} : ${sec}</p>`
 }
 
 
@@ -76,9 +107,10 @@ setInterval(() => {
     let min = time.getMinutes();
     let sec = time.getSeconds();
 
-    hour = hour <10 ? "0"+hour : hour;
-    min = min <10 ? "0"+min : min;
-    sec = sec <10 ? "0"+sec : sec;
+    hour = hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec;
 
-    clock(hour,min,sec)
+    clock(hour, min, sec)
 }, 1000);
+
